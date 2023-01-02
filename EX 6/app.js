3df71c24a8559006fdb2c9ead0a8cf706c7ec02b
Lorena <lorenagrazy@yahoo.com.br>
2023-01-02 17:28:28 -0300

    Salvar Exercício 6

---
+++ EX 6/app.js
@@ -0,0 +1,27 @@
+function verificaPalindromoFor(texto) {
+
+  const meio = texto.length / 2;
+  const fim = texto.length - 1;
+
+  for (let i = 0; i < meio; i++) {
+    if (texto[i] !== texto[fim - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+const verificaPalindromo = t => [...t].reverse().join('') === t;
+
+const teste0 = verificaPalindromo("osso");
+console.log(teste0);
+
+
+const teste1 = verificaPalindromo("ana");
+console.log(teste1);
+
+
+const teste2 = verificaPalindromo("julia");
+console.log(teste2);
